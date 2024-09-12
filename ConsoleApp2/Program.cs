aed571a73c46d1e4ee49c59e78757121a475ccb0
gerogrimaldi <gerogrimaldi2@gmail.com>
2024-09-12 10:35:49 -0300

    Ejercicios de clase 3

--- ConsoleApp2/Program.cs
+++ ConsoleApp2/Program.cs
@@ -1,4 +1,9 @@
-﻿internal class Program
+﻿using System.Drawing;
+using System.Linq.Expressions;
+using System.Net;
+using System.Text.RegularExpressions;
+
+internal class Program
 {
     private static void Main(string[] args)
     {
@@ -48,5 +53,89 @@
             //Console.WriteLine(item == null ? "Sin valor" : item);
             Console.WriteLine(item ?? "Sin Valor");
         }
+
+        Array.Resize(ref cadenasConNull, 2);
+
+        //########### EJERCICIOS CLASE 3 ##################
+
+        //Definir una variable numérica, asignarle un valor y sumarle 5.
+        int var1 = 0;
+        var1 += 5;
+
+        //Definir dos variables de cadenas, asignarles valores y concatenarlas.
+        Console.WriteLine("\n#############################################\n");
+        string cadena1 = "Hola";
+        string cadena2 = "Mundo";
+        Console.WriteLine("CADENAS: ");
+        Console.WriteLine(cadena1 + " " + cadena2);
+
+        //Evaluar si dos números son iguales, diferentes, mayor o menor.
+        Console.WriteLine("\n#############################################\n");
+        Console.WriteLine("Ingrese 2 valores");
+        
+        var1 = Convert.ToInt16(Console.ReadLine());
+        int var2 = Convert.ToInt16(Console.ReadLine());
+
+        Console.WriteLine($"Var1= {var1} Var2= {var2}");
+        Console.WriteLine((var1 != var2) ? "Los numeros NO son iguales" : "Los numeros son iguales"); 
+        Console.WriteLine((var1 > var2) ? 
+            "var1 es mayor" : (var1 < var2) ? 
+            "var2 es mayor" : "Los numeros son iguales");
+
+        //Definir una variable numérica.
+        //Asignarle un valor entre 1 y 10
+        //Mostrar a que grupo pertenece:
+
+        //Grupo 1: del 1 al 3
+        //Grupo 2: del 4 al 6
+        //Grupo 3: del 7 al 10
+        Console.WriteLine("\n#############################################\n");
+        Console.WriteLine("SWITCH: ");
+
+        Console.WriteLine("\nIngrese otro valor");
+        var2 = Convert.ToInt16(Console.ReadLine());
+        Console.WriteLine($"Nueva var2= {var2}");
+
+        switch(var2)
+        {
+            case int when (var2 <= 3 && var2 > 0):
+                Console.WriteLine("Grupo 1");
+                break;
+            case int when (var2 <= 6 && var2 > 3):
+                Console.WriteLine("Grupo 2");
+                break;
+            case int when (var2 <= 10 && var2 > 6):
+                Console.WriteLine("Grupo 3");
+                break;
+            default:
+                Console.WriteLine("Ningun grupo");
+                break;
+        }
+
+
+        //Realizar la sumatoria de 0 a 10 y devolver el valor de la misma.
+        Console.WriteLine("\n#############################################\n");
+        Console.WriteLine("WHILE: ");
+
+        int n = 0;
+        while (n < 10)
+        {
+            n+=1;
+        }
+            Console.WriteLine($"Sumatoria: {n}");
+
+        //Generar un array con 10 números, 
+        //recorrerlo e ir multiplicando todos los elementos, 
+        //finalmente obtener elproducto total.
+        Console.WriteLine("\n#############################################\n");
+        Console.WriteLine("ARRAY: ");
+
+        int[] numeros = new int[] { 1, 2, 3, 4, 5 ,6, 7, 8, 9, 10};
+        n = 1;
+        foreach (int nro in numeros)
+        {
+            n *= nro;
+        }
+        Console.WriteLine($"Factorial de 10: {n}");
     }
 }
